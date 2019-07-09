bf5f04eb53cf3d958201507b238efc8617abb923
Bjoern Wegner <bjoern.wegner@hotmail.com>
2019-07-09 20:20:59 +0200

    Spacing

--- src/NLog.Targets.Splunk/Splunk.Logging.Common/HttpEventCollectorSenderSettings.cs
+++ src/NLog.Targets.Splunk/Splunk.Logging.Common/HttpEventCollectorSenderSettings.cs
@@ -3,15 +3,15 @@ using System;
 
 namespace NLog.Targets.Splunk.Splunk.Logging.Common
 {
-  public class HttpEventCollectorSenderSettings
-  {
-    public string RenderedToken => Token.Render(LogEventInfo.CreateNullEvent());
-    public Uri RenderedUri => new Uri(Uri.Render(LogEventInfo.CreateNullEvent()));
+    public class HttpEventCollectorSenderSettings
+    {
+        public string RenderedToken => Token.Render(LogEventInfo.CreateNullEvent());
+        public Uri RenderedUri => new Uri(Uri.Render(LogEventInfo.CreateNullEvent()));
 
-    public Layout Uri { get; set; }
-    public Layout Token { get; set; }
-    public bool IgnoreSslErrors { get; set; } = false;
-    public bool UseProxy { get; set; } = false;
-    public int MaxConnectionsPerServer { get; set; } = 0;
-  }
+        public Layout Uri { get; set; }
+        public Layout Token { get; set; }
+        public bool IgnoreSslErrors { get; set; } = false;
+        public bool UseProxy { get; set; } = false;
+        public int MaxConnectionsPerServer { get; set; } = 0;
+    }
 }
