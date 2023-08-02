3e4ea5a65ee7fc30e42248141a1fa5b86d1a13d5
roshangavandi <37402152+roshangavandi@users.noreply.github.com>
2023-08-02 08:43:13 -0700

    Update build-nuget-packages.ps1

--- src/build-nuget-packages.ps1
+++ src/build-nuget-packages.ps1
@@ -4,6 +4,8 @@ Param(
 )
 cd .\NLog.Targets.Splunk
 Write-Output "Building release $versionSuffix nuget packages..."
+Write-Output "Restoring NuGet packages..."
+dotnet restore
 dotnet pack --configuration Release --include-symbols --version-suffix $versionSuffix
 Write-Output "Moving $versionSuffix nuget packages to releases folder..."
 Move-Item .\bin\Release\*.nupkg ..\..\releases -Force
